d06060e587656a5c2dfbdc45eb1e9bec8eca9edb
yaswant <2984440+yaswant@users.noreply.github.com>
2026-08-03 13:36:14 +0100

    Merge main

--- src/control/core/top_level/ukca_radaer_lfric_api_mod.F90
+++ src/control/core/top_level/ukca_radaer_lfric_api_mod.F90
@@ -1,8 +1,8 @@
-! *****************************COPYRIGHT*******************************
+! -----------------------------------------------------------------------------
 ! (C) Crown copyright Met Office. All rights reserved.
-! For further details please refer to the file COPYRIGHT.txt
-! which you should have received as part of this distribution.
-! *****************************COPYRIGHT*******************************
+! The file LICENCE, distributed with this code, contains details of the terms
+! under which the code may be used.
+! -----------------------------------------------------------------------------
 !
 !  An interface routine to pass chunked columns from lfric to radaer
 !
